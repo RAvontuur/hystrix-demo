51abf93303cbc1cef3839f4842417f722fb59181
Rene Avontuur <rene.avontuur@fdmediagroep.nl>
2017-07-18 00:12:33 +0200

    fallback as private method

--- src/main/java/myapp/MyServiceClient.java
+++ src/main/java/myapp/MyServiceClient.java
@@ -11,18 +11,18 @@ public class MyServiceClient {
     private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(MyServiceClient.class);
 
     @Autowired
-    private MyService myRemoteService;
+    private MyService myService;
 
     @HystrixCommand(
-            // fallbackMethod = "fallback",
+            fallbackMethod = "fallback",
             commandProperties = {
                     @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
             })
     public MyResult callService(long id, long sleep) {
-        return myRemoteService.retrieveResult(id, sleep);
+        return myService.retrieveResult(id, sleep);
     }
 
-    public MyResult fallback(long id, long sleep) {
+    private MyResult fallback(long id, long sleep) {
 
         MyResult myResult = new MyResult();
         myResult.setResult("Fallback: " + id);
